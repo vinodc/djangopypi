5d024cd28599bff6f80cf185f2098e5aaede7cc7
Jannis Leidel <jannis@leidel.info>
2010-06-16 20:27:16 +0800

    Updated setup file a little and moved doc files to root dir of djangopypi app.

--- src/djangopypi/setup.py
+++ src/djangopypi/setup.py
@@ -1,23 +1,17 @@
 import os
+from setuptools import setup, find_packages
 
-try:
-    from setuptools import setup, find_packages
-except ImportError:
-    from ez_setup import use_setuptools
-    use_setuptools()
-    from setuptools import setup, find_packages
-
-import os
+def fread(fname):
+    return open(os.path.join(os.path.dirname(__file__), '', fname)).read()
 
 version = '0.4'
 
-setup(name='djangopypi',
-      version=version,
-      description="A Django application that emulates the Python Package Index.",
-      long_description=open(os.path.join('docs', "README")).read() + "\n\n" +
-                       open(os.path.join('docs', 'Changelog')).read() + "\n\n" +
-                       open(os.path.join('docs', 'TODO')).read(),
-      classifiers=[
+setup(
+    name='djangopypi',
+    version=version,
+    description="A Django application that emulates the Python Package Index.",
+    long_description=fread("README")+"\n\n"+fread('Changelog')+"\n\n"+fread('TODO'),
+    classifiers=[
         "Framework :: Django",
         "Development Status :: 4 - Beta",
         #"Development Status :: 5 - Production/Stable",
@@ -29,19 +23,21 @@ setup(name='djangopypi',
         "License :: OSI Approved :: BSD License",
         "Topic :: System :: Software Distribution",
         "Programming Language :: Python",
-        "Topic :: Software Development :: Libraries :: Python Modules",],
-      keywords='django pypi packaging index',
-      author='Ask Solem',
-      author_email='askh@opera.com',
-      maintainer='Benjamin Liles',
-      maintainer_email='benliles@gmail.com',
-      url='http://github.com/benliles/chishop',
-      license=open(os.path.join("docs", "LICENSE")).read(),
-      packages=find_packages(),
-      include_package_data=True,
-      zip_safe=False,
-      install_requires=[
-          'setuptools',
-          'django>=1.0',
-          'docutils',],
-      )
+        "Topic :: Software Development :: Libraries :: Python Modules",
+    ],
+    keywords='django pypi packaging index',
+    author='Ask Solem',
+    author_email='askh@opera.com',
+    maintainer='Benjamin Liles',
+    maintainer_email='benliles@gmail.com',
+    url='http://github.com/benliles/chishop',
+    license='BSD',
+    packages=find_packages(),
+    include_package_data=True,
+    zip_safe=False,
+    install_requires=[
+        'setuptools',
+        'django>=1.0',
+        'docutils',
+    ],
+)
