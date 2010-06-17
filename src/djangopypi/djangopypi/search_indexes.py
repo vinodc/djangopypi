71a12a9ec4e327f67fa070484b74389315ef1354
Benjamin Liles <benliles@gmail.com>
2010-06-17 11:48:03 -0500

    Fixed a few typos/bugs with the Package search index

--- src/djangopypi/djangopypi/search_indexes.py
+++ src/djangopypi/djangopypi/search_indexes.py
@@ -9,19 +9,19 @@ if 'haystack' in settings.INSTALLED_APPS:
 
     class PackageSearchIndex(SearchIndex):
         name = CharField(model_attr='name')
-        text = CharField(document=True, use_template=True, null=True,
+        text = CharField(document=True, use_template=True, null=True, stored=False,
                          template_name='djangopypi/haystack/package_text.txt')
-        author = MultiValueField(store=False, null=True)
-        classifier = MultiValueField(store=False, null=True,
+        author = MultiValueField(stored=False, null=True)
+        classifier = MultiValueField(stored=False, null=True,
                                      model_attr='latest__classifiers')
-        summary = CharField(store=False, null=True,
+        summary = CharField(stored=False, null=True,
                             model_attr='latest__summary')
-        description = CharField(store=False, null=True,
+        description = CharField(stored=False, null=True,
                                 model_attr='latest__description')
         
         def prepare_author(self, obj):
             output = []
-            for user in obj.owners.all() + obj.maintainers.all():
+            for user in list(obj.owners.all()) + list(obj.maintainers.all()):
                 output.append(user.get_full_name())
                 if user.email:
                     output.append(user.email)
