de69a553e7b1e0684d2bd35113000a975bf85571
Benjamin Liles <benliles@gmail.com>
2010-06-17 11:53:46 -0500

    Version 0.4.1

--- src/djangopypi/setup.py
+++ src/djangopypi/setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 def fread(fname):
     return open(os.path.join(os.path.dirname(__file__), fname)).read()
 
-version = '0.4'
+version = '0.4.1'
 
 setup(
     name='djangopypi',
